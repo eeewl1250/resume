2395fb10c3129b4e747b74155d54182025d69f49
Wang <eeewl1250@163.com>
2019-02-22 22:44:52 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  // vue-cli3部署：
+  // https://cli.vuejs.org/zh/guide/deployment.html#github-pages
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/resume/'
+      : '/',
   outputDir: 'docs'
 }
